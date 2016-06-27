1233b18627530057ba38a4f192d70c537c6b4b5f
Ricardo Centeno Lugo <Rick@mbpdericardo2.gvcorp.org>
2016-06-27 15:10:51 -0600

    HonduContacts start form

--- HonduContact/app/src/main/java/com/mobintum/honducontact/fragments/ContactsRVFragment.java
+++ HonduContact/app/src/main/java/com/mobintum/honducontact/fragments/ContactsRVFragment.java
@@ -3,16 +3,22 @@ package com.mobintum.honducontact.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.mobintum.honducontact.R;
+import com.mobintum.honducontact.adapters.ContactRVAdapter;
+import com.mobintum.honducontact.models.Contact;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContactsRVFragment extends Fragment {
+
+    RecyclerView rvContacts;
 
 
     public ContactsRVFragment() {
@@ -23,8 +29,13 @@ public class ContactsRVFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts_rv, container, false);
+        View view = inflater.inflate(R.layout.fragment_contacts_rv, container, false);
+        rvContacts = (RecyclerView) view.findViewById(R.id.rvContacts);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
+        rvContacts.setLayoutManager(layoutManager);
+        ContactRVAdapter adapter = new ContactRVAdapter(Contact.getContacts(getContext()));
+        rvContacts.setAdapter(adapter);
+        return view;
     }
 
 }
---
+++ HonduContact/app/src/main/java/com/mobintum/honducontact/fragments/InputContactFragment.java
@@ -0,0 +1,29 @@
+package com.mobintum.honducontact.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mobintum.honducontact.R;
+
+
+public class InputContactFragment extends Fragment {
+
+
+    public InputContactFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_input_contact, container, false);
+    }
+
+}
